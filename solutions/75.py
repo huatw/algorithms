0e183388c761ad617938bd05a69f62a75e2f3e49
huatw <huastron@gmail.com>
2018-02-21 23:19:50 -0500

    75

---
+++ solutions/75.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortColors(self, nums):
+        red, white, blue = 0, 0, len(nums)-1
+
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                red += 1
+                white += 1
+            elif nums[white] == 2:
+                nums[blue], nums[white] = nums[white], nums[blue]
+                blue -= 1
+            else:
+                white += 1
+
+
