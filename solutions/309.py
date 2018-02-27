bfd136c60a3fb6a47d2390a16cd99ced9375b802
huatw <huastron@gmail.com>
2018-02-27 17:56:47 -0500

    309

---
+++ solutions/309.py
@@ -0,0 +1,15 @@
+# DP
+# free cool - free
+# free hold - hold
+# hold - cool
+class Solution(object):
+    def maxProfit(self, prices):
+        free, cool, hold = 0, -float('inf'), -float('inf')
+
+        for price in prices:
+            free, cool, hold = max(free, cool), hold + price, max(free - price, hold)
+
+        return max(free, cool)
+
+
+
