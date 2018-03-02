f3dfca1fba937d1e8481aec34eb2102dd1ac6d1f
huatw <huastron@gmail.com>
2018-03-02 00:58:10 -0500

    243

---
+++ solutions/243.py
@@ -0,0 +1,14 @@
+# DP
+class Solution(object):
+    def uniquePaths(self, m, n):
+        res = [[0] * (n + 1) for _ in range(m + 1)]
+        res[0][1] = 1
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                res[i][j] = res[i-1][j] + res[i][j-1]
+
+        return res[i][j]
+
+
+
