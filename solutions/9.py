eecab8893af93457b40f8f596f10d3e83c69efde
huatw <huastron@gmail.com>
2018-03-11 23:50:46 -0400

    9

---
+++ solutions/9.py
@@ -0,0 +1,45 @@
+class Solution:
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+
+        return int(str(x)[::-1]) == x
+
+
+
+
+class Solution:
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+
+        ranger = 1
+
+        while x >= ranger * 10:
+            ranger *= 10
+
+        while x:
+            left, right = x // ranger, x % 10
+            if left != right:
+                return False
+            x = (x % ranger) // 10
+            ranger //= 100
+
+        return True
+
+
+
+        ranger = 1
+        while x / ranger >= 10:
+            ranger *= 10
+
+        while x:
+            left = x / ranger
+            right = x % 10
+            if left != right:
+                return False
+
+            x = (x % ranger) / 10
+            ranger /= 100
+
+        return True
