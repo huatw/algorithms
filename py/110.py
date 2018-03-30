98f9ea9f648d5f8b3b8192bb8723896949d4f607
huatw <huastron@gmail.com>
2018-03-30 15:06:10 -0400

    110 DFS

--- py/110.py
+++ py/110.py
@@ -1,27 +1,22 @@
 # DFS
 class Solution(object):
     def isBalanced(self, root):
-        if not root:
-            return True
-
         def check(root):
             if not root:
                 return 0
 
-            l_depth = check(root.left)
-            if l_depth is False:
-                return False
-
-            r_depth = check(root.right)
-            if r_depth is False:
-                return False
+            l_depth, r_depth = check(root.left), check(root.right)
 
             if abs(l_depth - r_depth) > 1:
-                return False
+                raise
 
             return max(l_depth, r_depth) + 1
 
-        return check(root) is not False
+        try:
+            check(root)
+            return True
+        except:
+            return False
 
 
 
