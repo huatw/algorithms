fd71a09bc8ffe7b8043693b029eef326782c7ee0
huatw <huastron@gmail.com>
2018-08-13 15:45:40 -0400

    162 binary

---
+++ leetcode/py/162.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findPeakElement(self, nums):
+        def search(start, end):
+            if start == end:
+                return start
+            mid = (start + end) // 2
+            if nums[mid] > nums[mid + 1]:
+                return search(start, mid)
+            return search(mid + 1, end)
+
+        return search(0, len(nums) - 1)
+
+
+class Solution:
+    def findPeakElement(self, nums):
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
